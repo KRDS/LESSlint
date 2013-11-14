26007cdb20262bef89bf117b3a5d54772348f3d0
ipernet <ipernet@gmail.com>
2013-11-14 08:53:26 +0100

    Fix Issue #8

--- checks/NoUnitToZero.php
+++ checks/NoUnitToZero.php
@@ -4,7 +4,7 @@ class Checks_NoUnitToZero
 {
 	public function check($line)
 	{
-		if(preg_match('#[^0-9]0([\%|in|cm|mm|em|ex|pt|pc|px|]+)#', $line))
+		if(preg_match('#[^0-9a-f\#]0([\%|in|cm|mm|em|ex|pt|pc|px|]+)#', $line))
 			return 'No need to specify units when a value is 0';
 	}
 }
