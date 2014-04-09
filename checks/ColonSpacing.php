b168f7408177170a8191108a60d2a056f82097fc
ipernet <ipernet@gmail.com>
2014-04-09 11:46:31 +0200

    Fix issue #10 (:extend not accepted)

--- checks/ColonSpacing.php
+++ checks/ColonSpacing.php
@@ -4,7 +4,7 @@ class Checks_ColonSpacing
 {
 	public function check($line)
 	{
-		if(strpos($line, ':') !== false && strpos($line, '{') === false && ! preg_match('#([^\s]):([ ]{1})#', $line))
+		if(strpos($line, ':') !== false && strpos($line, '{') === false && strpos($line, ':extend') === false && ! preg_match('#([^\s]):([ ]{1})#', $line))
 			return 'Colon should have no space before, and one after';
 	}
 }
